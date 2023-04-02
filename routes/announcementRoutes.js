bad0b4a620c60645f3fe7b905a34261d378bf1c2
IsaaacLim <isaaclimjj@gmail.com>
2023-04-02 17:50:45 +0800

    feat: api call require auth

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const announcementsController = require("../controllers/announcementsController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router
   .route("/")
