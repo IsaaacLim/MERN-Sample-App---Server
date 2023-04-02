b7b956328cb28b21882b587df5fde6f21d1625db
IsaaacLim <isaaclimjj@gmail.com>
2023-04-02 21:29:17 +0800

    chore: announcements can be fetched by all users

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const announcementsController = require("../controllers/announcementsController");
 const verifyJWT = require("../middleware/verifyJWT");
 
+router.route("/").get(announcementsController.getAllAnnouncements);
+
 router.use(verifyJWT);
 
 router
   .route("/")
-  .get(announcementsController.getAllAnnouncements)
   .post(announcementsController.createNewAnnouncement)
   .patch(announcementsController.updateAnnouncement)
   .delete(announcementsController.deleteAnnouncement);
