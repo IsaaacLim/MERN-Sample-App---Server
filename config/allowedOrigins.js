d05f256c143ba13f25e9296c14d5d4c520e67cf9
IsaaacLim <isaaclimjj@gmail.com>
2023-04-04 04:16:14 +0800

    build: fix allow Vercel deployment cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000", // remove to prevent others from accessing from their localhost
   "https://mern-sample-app.onrender.com",
-  "https://mern-sample-app-client.vercel.app/",
+  "https://mern-sample-app-client.vercel.app",
 ];
 
 module.exports = allowedOrigins;
